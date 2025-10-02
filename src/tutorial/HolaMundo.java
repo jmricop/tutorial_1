64341cc5bfc132263879739f5551b105af3daed9
jmricop <jmricop01@gmail.com>
2025-10-02 11:16:05 +0200

    Segundo commit en branch development

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola CMEPPS!");
 		System.out.println("Estamos en la branch development");
+		System.out.println("Vamos ha hacer el segundo commit en la branch development");
 	}
 
 }
