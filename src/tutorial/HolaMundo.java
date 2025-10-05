12369fe48e0ad1189a6cb78f9a5bc808c546b9aa
jmricop <jmricop01@gmail.com>
2025-10-05 20:43:27 +0200

    Punto 8

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -7,6 +7,8 @@ public class HolaMundo {
 		System.out.println("Estamos en la branch development");
 		System.out.println("Vamos ha hacer el segundo commit en la branch development");
 		System.out.println("Esta es una nueva brand (punto 5");
+		System.out.println("Esta es una nuevo commit, punto 8");
+
 	}
 
 }
